e851de47dfd8969a7c8f1050bce7047344cc5a03
Juan Diego Guerra <jdxelric@gmail.com>
2024-10-17 03:04:28 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
+from backend.api import recordatorios
 
+# Inicialización de la aplicación
 app = FastAPI()
 
+# Incluir los routers
+app.include_router(recordatorios.router, prefix="/api")
+
+# Endpoints simples
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
 
-# Ruta para verificar que la aplicación está corriendo correctamente
 @app.get("/health")
 def health_check():
     return {"status": "Healthy"}
