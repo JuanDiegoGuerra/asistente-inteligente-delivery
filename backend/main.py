8676c86e088368503d9ef14b114db52af5349492
Juan Diego Guerra <jdxelric@gmail.com>
2024-10-23 04:30:58 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from backend.api import recordatorios, promociones
+from fastapi.middleware.cors import CORSMiddleware
 
 # Inicialización de la aplicación
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000/"],  # Cambia "*" por la URL de tu frontend en producción
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Incluir los routers
 app.include_router(recordatorios.router, prefix="/api")
